13d2e0d5810e6879dd9315f03d06337f63a2b679
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-08-22 16:00:49 -0300

    interface CRUD criada

--- src/main/java/br/com/springboot/model/Cliente.java
+++ src/main/java/br/com/springboot/model/Cliente.java
@@ -1,13 +1,41 @@
 package br.com.springboot.model;
 
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+@Entity
+@Table(name = "clientes")
+
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, length = 50)
     private String nome;
+    @Column(length = 11)
     private String cpf;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Column(name = "data_nascimento", columnDefinition = "DATE")
     private LocalDate dataDeNascimento;
+    @Enumerated(EnumType.STRING)
     private Sexo sexo;
+    @Column(length = 10)
     private String telefone;
+    @Column(length = 11)
     private String celular;
+    @Column(length = 50)
     private String email;
     private boolean ativo;
 
