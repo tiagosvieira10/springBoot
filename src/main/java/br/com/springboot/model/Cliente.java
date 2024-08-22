8cc93018c04001b08c15e211c4284a64f1e1ef15
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-08-22 14:07:21 -0300

    project springboot

---
+++ src/main/java/br/com/springboot/model/Cliente.java
@@ -0,0 +1,15 @@
+package br.com.springboot.model;
+
+public class Cliente {
+    private Long id;
+    private String nome;
+    private String cpf;
+    private LocalDate dataDeNascimento;
+    private Sexo sexo;
+    private String telefone;
+    private String celular;
+    private String email;
+    private boolean ativo;
+
+}
+
---
+++ src/main/java/br/com/springboot/model/LocalDate.java
@@ -0,0 +1,5 @@
+package br.com.springboot.model;
+
+public class LocalDate {
+
+}
