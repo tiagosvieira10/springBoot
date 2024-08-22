83357581f18e839b14796f6b82ba0e3fd096873e
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-08-22 14:39:55 -0300

    implementacao modelo de dados

--- src/main/java/br/com/springboot/model/Cliente.java
+++ src/main/java/br/com/springboot/model/Cliente.java
@@ -11,5 +11,73 @@ public class Cliente {
     private String email;
     private boolean ativo;
 
+    public Long getId() {
+      return id;
+    }
+
+    public String getNome() {
+      return nome;
+    }
+
+    public void setNome(String nome) {
+      this.nome = nome;
+    }
+
+    public String getCpf() {
+      return cpf;
+    }
+
+    public void setCpf(String cpf) {
+      this.cpf = cpf;
+    }
+
+    public LocalDate getDataDeNascimento() {
+      return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+      this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public Sexo getSexo() {
+      return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+      this.sexo = sexo;
+    }
+
+    public String getTelefone() {
+      return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+      this.telefone = telefone;
+    }
+
+    public String getCelular() {
+      return celular;
+    }
+
+    public void setCelular(String celular) {
+      this.celular = celular;
+    }
+
+    public String getEmail() {
+      return email;
+    }
+
+    public void setEmail(String email) {
+      this.email = email;
+    }
+
+    public boolean isAtivo() {
+      return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+      this.ativo = ativo;
+    }
+
 }
 
